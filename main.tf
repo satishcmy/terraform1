c18366b6a6f04f4a04b98c7a1fa0be7cbe334ead
satishcmy <55646893+satishcmy@users.noreply.github.com>
2025-01-22 13:38:46 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "web_sg" {
 
 # Web server in public subnet
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+  ami                    = "ami-0fc0946ac74e8e050" # Amazon Linux 2 in us-east-1
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
