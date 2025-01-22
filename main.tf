e961b56d91fef7b8c24c36f45f89550c2c9a167b
satishcmy <55646893+satishcmy@users.noreply.github.com>
2025-01-22 13:53:51 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_security_group" "db_sg" {
 
 # Database server in private subnet (no public IP)
 resource "aws_instance" "db_server" {
-  ami                    = "ami-0c02fb55956c7d316"
+  ami                    = "ami-0fc0946ac74e8e050"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
